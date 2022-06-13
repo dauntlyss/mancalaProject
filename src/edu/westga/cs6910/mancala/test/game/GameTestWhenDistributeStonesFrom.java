848f37e9e2a13004cb4af8897fe779e7373e349e
Alyssa Harris <ah00214@my.westga.edu>
2022-06-13 14:12:21 -0400

    Completed and passes test for distributeStonesFrom()

--- src/edu/westga/cs6910/mancala/test/game/GameTestWhenDistributeStonesFrom.java
+++ src/edu/westga/cs6910/mancala/test/game/GameTestWhenDistributeStonesFrom.java
@@ -1,6 +1,7 @@
 package edu.westga.cs6910.mancala.test.game;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
@@ -31,7 +32,7 @@ public class GameTestWhenDistributeStonesFrom {
 	}
 	
 	/**
-	 * Test method for distributeStonesFrom(int) method.
+	 * Test method for distributeStonesFrom(int) method, tests that stones go from pit 2 to store.
 	 */
 	@Test
 	public void testDistributeStonesFromDistributesStonesFromPit2ToStore() {
@@ -46,18 +47,86 @@ public class GameTestWhenDistributeStonesFrom {
 	}
 	
 	/**
-	 * Test method for distributeStonesFrom(int) method.
+	 * Test method for distributeStonesFrom(int) method, tests that 2 accumulated stones go from pit 2 to pit 4.
 	 */
 	@Test
-	public void testDistributeStonesFromDistributesStonesFromPit2ToStorse() {
+	public void testDistributeStonesFromDistributes2StonesFromPit2IntoStoreAndPit4() {
 		Game newGame = new Game();
 		
 		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
 		newGame.startNewGame(simpleComputer);
 		
+		newGame.distributeStonesFrom(1);
 		newGame.distributeStonesFrom(2);
-		assertEquals(1, newGame.getStones(3));
+		assertEquals(2, newGame.getStones(4));
 		
 	}
+	
+	/**
+	 * Test method for distributeStonesFrom(int) method, tests that stones go from pit 6 to store.
+	 */
+	@Test
+	public void testDistributeStonesFromDistributesStonesFromPit6ToStore() {
+		Game newGame = new Game();
+		
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		
+		newGame.distributeStonesFrom(6);
+		assertEquals(1, newGame.getStones(7));
+		
+	}
+	
+	/**
+	 * Test method for distributeStonesFrom(int) method, tests that 2 accumulated stones go from pit 6 to pit 0.
+	 */
+	@Test
+	public void testDistributeStonesFromDistributes2StonesFromPit6IntoStoreAndPit0() {
+		Game newGame = new Game();
+		
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		
+		newGame.distributeStonesFrom(5);
+		newGame.distributeStonesFrom(6);
+		assertEquals(2, newGame.getStones(0));
+		
+	}
+	
+	/**
+	 * Test method for distributeStonesFrom(int) method, tests that when stones are distributed from a pit the pit is left empty.
+	 */
+	@Test
+	public void testDistributeStonesFromDistributesStonesFromPit0AndLeavesPitEmpty() {
+		Game newGame = new Game();
+		
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		
+		newGame.distributeStonesFrom(0);
+		assertEquals(0, newGame.getStones(0));
+		
+	}
+	
+	/**
+	 * Test method for distributeStonesFrom(int) method, tests error handling.
+	 */
+	@Test
+	public void testDistributeStonesFromThrowsProperErrorIfNegativePitNumberIsPassed() {
+		Game newGame = new Game();
+		
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		
+		try {
+	        newGame.distributeStonesFrom(-1);     
+	        fail();
+	    } catch (IllegalArgumentException ex) {
+	        assertEquals("Pit number cannot be negative", ex.getMessage());
+	    }
+		
+	}
+	
+	
 
 }
