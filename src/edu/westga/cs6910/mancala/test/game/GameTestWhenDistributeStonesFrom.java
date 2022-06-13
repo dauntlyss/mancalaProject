a3fecc1eabb132978f8387333dc2557ee4960fe4
Alyssa Harris <ah00214@my.westga.edu>
2022-06-13 13:44:50 -0400

    Continuing testing distribute method

--- src/edu/westga/cs6910/mancala/test/game/GameTestWhenDistributeStonesFrom.java
+++ src/edu/westga/cs6910/mancala/test/game/GameTestWhenDistributeStonesFrom.java
@@ -1,23 +1,63 @@
 package edu.westga.cs6910.mancala.test.game;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs6910.mancala.model.ComputerPlayer;
+import edu.westga.cs6910.mancala.model.Game;
+
 /**
- * [Description...]
+ * Test to confirm the game class is working as expected.
  *
  * @author Alyssa Harris
  * @version Jun 13, 2022
  */
-class GameTestWhenDistributeStonesFrom {
+public class GameTestWhenDistributeStonesFrom {
 
 	/**
-	 * Test method for {@link edu.westga.cs6910.mancala.model.Game#distributeStonesFrom(int)}.
+	 * Test method for distributeStonesFrom(int) method.
 	 */
 	@Test
-	void testDistributeStonesFrom() {
-		fail("Not yet implemented");
+	public void testDistributeStonesFromDistributesStonesFromPit0ToPit1() {
+		Game newGame = new Game();
+		
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		
+		newGame.distributeStonesFrom(0);
+		assertEquals(2, newGame.getStones(1));
+		
+	}
+	
+	/**
+	 * Test method for distributeStonesFrom(int) method.
+	 */
+	@Test
+	public void testDistributeStonesFromDistributesStonesFromPit2ToStore() {
+		Game newGame = new Game();
+		
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		
+		newGame.distributeStonesFrom(2);
+		assertEquals(1, newGame.getStones(3));
+		
+	}
+	
+	/**
+	 * Test method for distributeStonesFrom(int) method.
+	 */
+	@Test
+	public void testDistributeStonesFromDistributesStonesFromPit2ToStorse() {
+		Game newGame = new Game();
+		
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		
+		newGame.distributeStonesFrom(2);
+		assertEquals(1, newGame.getStones(3));
+		
 	}
 
 }
