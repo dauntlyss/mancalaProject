6b7b945132686ef18a9b085ae096f772928685b9
Alyssa Harris <ah00214@my.westga.edu>
2022-06-13 13:16:42 -0400

    Started testing distributeStonesFrom method

---
+++ src/edu/westga/cs6910/mancala/test/game/GameTestWhenDistributeStonesFrom.java
@@ -0,0 +1,23 @@
+package edu.westga.cs6910.mancala.test.game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * [Description...]
+ *
+ * @author Alyssa Harris
+ * @version Jun 13, 2022
+ */
+class GameTestWhenDistributeStonesFrom {
+
+	/**
+	 * Test method for {@link edu.westga.cs6910.mancala.model.Game#distributeStonesFrom(int)}.
+	 */
+	@Test
+	void testDistributeStonesFrom() {
+		fail("Not yet implemented");
+	}
+
+}
