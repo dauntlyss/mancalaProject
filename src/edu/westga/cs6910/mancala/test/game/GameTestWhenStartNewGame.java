f5e251fb843e2b299e4df2ca93ede09221271790
Alyssa Harris <ah00214@my.westga.edu>
2022-06-13 13:15:57 -0400

    Tested and passed startNewGame method

--- src/edu/westga/cs6910/mancala/test/game/GameTestWhenStartNewGame.java
+++ src/edu/westga/cs6910/mancala/test/game/GameTestWhenStartNewGame.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import edu.westga.cs6910.mancala.model.ComputerPlayer;
 import edu.westga.cs6910.mancala.model.Game;
+import edu.westga.cs6910.mancala.model.HumanPlayer;
 
 /**
  * Test to confirm the game class is working as expected.
@@ -25,6 +26,52 @@ public class GameTestWhenStartNewGame {
 		newGame.startNewGame(simpleComputer);
 		assertEquals(simpleComputer, newGame.getCurrentPlayer());
 		
+	}
+	
+	/**
+	 * Test method for Game classes startNewGame() method.
+	 */
+	@Test
+	public void testStartNewGameWillStartNewGameWithHumanFirst() {
+		Game newGame = new Game();
+		HumanPlayer realHumanPlayer = new HumanPlayer("Alyssa", newGame);
+		newGame.startNewGame(realHumanPlayer);
+		assertEquals(realHumanPlayer, newGame.getCurrentPlayer());
+		
+	}
+	
+	/**
+	 * Test method for Game classes startNewGame() method.
+	 */
+	@Test
+	public void testStartNewGameWillStartNewGameWithAllPitsHavingOneStoneEach() {
+		Game newGame = new Game();
+		
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		
+		for (int pitNumber = 0; pitNumber < 7; pitNumber++) {
+			if (pitNumber != 3) {
+				assertEquals(1, newGame.getStones(pitNumber));
+			}
+		}
+	}
+	
+	/**
+	 * Test method for Game classes startNewGame() method.
+	 */
+	@Test
+	public void testStartNewGameWillStartNewGameWithAllStoresHavingZeroStonesEach() {
+		Game newGame = new Game();
+		
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		
+		for (int pitNumber = 0; pitNumber <= 7; pitNumber++) {
+			if (pitNumber == 3 || pitNumber == 7) {
+				assertEquals(0, newGame.getStones(pitNumber));
+			}
+		}
 	}
 
 }
