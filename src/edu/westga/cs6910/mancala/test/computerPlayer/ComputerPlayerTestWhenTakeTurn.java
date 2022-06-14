f1a85a79a3b572f6b5479c45a94cd683cc928ec4
Alyssa Harris <ah00214@my.westga.edu>
2022-06-14 15:14:49 -0400

    Started Testing takeTurn()

---
+++ src/edu/westga/cs6910/mancala/test/computerPlayer/ComputerPlayerTestWhenTakeTurn.java
@@ -0,0 +1,29 @@
+package edu.westga.cs6910.mancala.test.computerPlayer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.mancala.model.ComputerPlayer;
+import edu.westga.cs6910.mancala.model.Game;
+
+/**
+ * Test to ensure the computerPlayer object is working correctly.
+ *
+ * @author Alyssa Harris
+ * @version Jun 14, 2022
+ */
+public class ComputerPlayerTestWhenTakeTurn {
+
+	@Test
+	public void testTakeTurnMethodTakesFromPit6OnFirstTurn() {
+		Game newGame = new Game();
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		simpleComputer.takeTurn(6);
+		int[] gameBoard = newGame.getGameBoard();
+		
+		assertEquals(0, gameBoard[6]);
+	}
+
+}
