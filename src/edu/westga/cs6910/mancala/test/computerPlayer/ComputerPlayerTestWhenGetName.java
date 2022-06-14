cdb86493dc3226e105742018e56e1d5f237958b7
Alyssa Harris <ah00214@my.westga.edu>
2022-06-14 10:51:25 -0400

    Started Testing getname()

---
+++ src/edu/westga/cs6910/mancala/test/computerPlayer/ComputerPlayerTestWhenGetName.java
@@ -0,0 +1,23 @@
+package edu.westga.cs6910.mancala.test.computerPlayer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * [Description...]
+ *
+ * @author Alyssa Harris
+ * @version Jun 14, 2022
+ */
+class ComputerPlayerTestWhenGetName {
+
+	/**
+	 * Test method for {@link edu.westga.cs6910.mancala.model.AbstractPlayer#getName()}.
+	 */
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+}
