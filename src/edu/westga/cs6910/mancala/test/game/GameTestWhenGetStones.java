7dbe22fcf17fd8fb122cf3578c763d1dc5c7d0b2
Alyssa Harris <ah00214@my.westga.edu>
2022-06-13 20:11:15 -0400

    Tested and passed getStonees()

---
+++ src/edu/westga/cs6910/mancala/test/game/GameTestWhenGetStones.java
@@ -0,0 +1,83 @@
+package edu.westga.cs6910.mancala.test.game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.mancala.model.ComputerPlayer;
+import edu.westga.cs6910.mancala.model.Game;
+import edu.westga.cs6910.mancala.model.HumanPlayer;
+
+/**
+ * Test to confirm the game class is working as expected.
+ *
+ * @author Alyssa Harris
+ * @version Jun 13, 2022
+ */
+public class GameTestWhenGetStones {
+
+	/**
+	 * Test method for testing the getStones(int) method, before the start of each game each pit amount can be retrieved and is 0.
+	 */
+	@Test
+	public void testGetStonesBeforeStartWillReturnZeroStonesPerPit() {
+		Game newGame = new Game();
+		assertEquals(0, newGame.getStones(0));
+	}
+	
+	/**
+	 * Test method for testing the getStones(int) method, test that at the start of each game each pit amount can be retrieved and is 1.
+	 */
+	@Test
+	public void testGetStonesAtStartWillReturnOneStonePerPit() {
+		Game newGame = new Game();
+		HumanPlayer simpleHuman = newGame.getHumanPlayer();
+		
+		newGame.startNewGame(simpleHuman);
+		
+		for (int pitNumber = 0; pitNumber < 7; pitNumber++) {
+			if (pitNumber != 3) {
+				assertEquals(1, newGame.getStones(pitNumber));
+			}
+		}
+		
+	}
+	
+	/**
+	 * Test method for Game classes getStones() method, test that before the start of each game each store amount can be retrieved and is 0.
+	 */
+	@Test
+	public void testStartNewGameWillStartNewGameWithAllStoresHavingZeroStonesEach() {
+		Game newGame = new Game();
+		
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		
+		for (int pitNumber = 0; pitNumber <= 7; pitNumber++) {
+			if (pitNumber == 3 || pitNumber == 7) {
+				assertEquals(0, newGame.getStones(pitNumber));
+			}
+		}
+	}
+	
+	/**
+	 * Test method Game classes getStones() method, tests error handling.
+	 */
+	@Test
+	public void testDistributeStonesFromThrowsProperErrorIfNegativePitNumberIsPassed() {
+		Game newGame = new Game();
+		
+		ComputerPlayer simpleComputer = new ComputerPlayer(newGame);
+		newGame.startNewGame(simpleComputer);
+		
+		try {
+	        newGame.getStones(-1);     
+	        fail();
+	    } catch (IllegalArgumentException ex) {
+	        assertEquals("Pit number cannot be negative", ex.getMessage());
+	    }
+		
+	}
+	
+}
