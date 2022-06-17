b4f52a0273236abcc922c6b1baf6255ab72fb88a
Alyssa Harris <ah00214@my.westga.edu>
2022-06-17 10:57:29 -0400

    Created the near strategy implementation

---
+++ src/edu/westga/cs6910/mancala/model/strategies/NearStrategy.java
@@ -0,0 +1,23 @@
+package edu.westga.cs6910.mancala.model.strategies;
+
+/**
+ * Implements the game-play strategy that always selects the nearest
+ * pit that has stones.
+ *
+ * @author Alyssa Harris
+ * @version Jun 17, 2022
+ */
+public class NearStrategy implements SelectStrategy {
+
+	@Override
+	public int selectPit(int[] gameBoard) {
+		int pitChoice = gameBoard.length - 2;
+		
+		if (gameBoard[pitChoice] == 0) {
+			pitChoice--;
+		}
+		
+		return pitChoice;
+	}
+
+}
