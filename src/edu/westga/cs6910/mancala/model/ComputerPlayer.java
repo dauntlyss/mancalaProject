febfea46b215f22f4640a8651a5f02c0fe3ec0dc
Alyssa Harris <ah00214@my.westga.edu>
2022-06-19 20:05:09 -0400

    Modified takeTurn and added setStrategy() to use new strategies

--- src/edu/westga/cs6910/mancala/model/ComputerPlayer.java
+++ src/edu/westga/cs6910/mancala/model/ComputerPlayer.java
@@ -1,5 +1,8 @@
 package edu.westga.cs6910.mancala.model;
 
+import edu.westga.cs6910.mancala.model.strategies.NearStrategy;
+import edu.westga.cs6910.mancala.model.strategies.SelectStrategy;
+
 /**
  * ComputerPlayer represents a very simple automated player in the game Mancala.
  * It always selects the closest pit for stones to be distributed
@@ -9,15 +12,44 @@ package edu.westga.cs6910.mancala.model;
  */
 public class ComputerPlayer extends AbstractPlayer {
 	private static final String NAME = "Simple computer";
+	private SelectStrategy strategy;
 
 	/**
 	 * Creates a new ComputerPlayer with the specified name.
 	 * 
 	 * @param	theGame	The Game that this player represents
-	 * 
 	 */
 	public ComputerPlayer(Game theGame) {
 		super(NAME, theGame);	
+		this.strategy = new NearStrategy();
+		this.setStrategy(this.strategy);
+	}
+	
+	@Override
+	/**
+	 * @see Player#takeTurn(int)
+	 */
+	public void takeTurn(int pitChoice) {
+		pitChoice = this.strategy.selectPit(this.getGame().getGameBoard());
+		
+		this.getGame().distributeStonesFrom(pitChoice);
+
+		this.setIsMyTurn(false);
+	}
+	
+	/**
+	 * Sets which strategy the player will use
+	 * 
+	 * @param someStrategy
+	 * 
+	 * @requires	someStrategy != null
+	 * @ensures	specified strategy will determine how the player will play
+	 */
+	public void setStrategy(SelectStrategy someStrategy) {
+		if (someStrategy == null) {
+			throw new IllegalArgumentException("Invalid strategy.");
+		}
+		this.strategy = someStrategy;
 	}
 
 }
